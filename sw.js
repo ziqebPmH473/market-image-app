20dbe6001e1dab84db55f84e138129bbb4895626
ziqebPmH473 <221719315+ziqebPmH473@users.noreply.github.com>
2026-03-05 21:11:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "market-image-cache"; // 固定名でOK
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
